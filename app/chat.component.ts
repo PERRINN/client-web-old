1cd1636b5157f5bbe351168db54c14a424fde1cf
Alessandro Silacci <silacci.alessandro@gmail.com>
2017-07-12 09:25:05 +0200

    Missing file in last commit

--- app/chat.component.ts
+++ app/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'chat',
